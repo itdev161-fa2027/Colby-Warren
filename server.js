0d90f6bf703ac118f613a318de7c3a2bf95aa2a3
Colby Warren <warrenc@matc.edu>
2026-09-19 00:15:30 -0500

    Add user registration validation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectDatabase from "./config/db.js";
+import { check, validationResult } from "express-validator";
 
 // Initialize express application
 const app = express();
@@ -19,10 +20,27 @@ app.get("/", (req, res) => {
  * @route POST api/users
  * @desc Register user
  */
-app.post("/api/users", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
-});
+app.post(
+  "/api/users",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters",
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      // Later we will save the user to the database here
+      return res.send(req.body);
+    }
+  },
+);
 
 // Connection listener
 app.listen(3000, () => console.log("Express server running on port 3000"));
