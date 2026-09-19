7c3de2bb85aed314bfed998e2949bb07654f5bd1
Colby Warren <warrenc@matc.edu>
2026-09-18 22:33:54 -0500

    Add user registration API endpoint

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ const app = express();
 // Connect to the database
 connectDatabase();
 
+// Configure Middleware
+app.use(express.json());
+
 // API endpoints
 app.get("/", (req, res) => {
   res.send("http get request sent to root api endpoint");
+});
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 // Connection listener
